54896c84643cc29becdb3c926a6591c3d9e415a1
reddy-gowthami <159762464+reddy-gowthami@users.noreply.github.com>
2024-12-03 12:34:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import NewsList from './components/NewsList';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <NewsList />
+    </>
+  );
+};
+
+export default App;
